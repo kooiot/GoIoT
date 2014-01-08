4b67d1b3737e0a4f8dd48750613e56a483dab545
cch <srdgame@gmail.com>
2014-01-08 16:53:36 +0800

    rename the configs to config, and improve the reply parse

--- web/core/configs/api.lua
+++ web/core/configs/api.lua
@@ -3,10 +3,13 @@ local cjson = require 'cjson.safe'
 cgilua.contentheader('application', 'text; charset=utf8')
 
 if cgilua.POST.key then
-	local api = require 'shared.api.configs'
+	local api = require 'shared.api.config'
 	local vals, err = cjson.decode(cgilua.POST.value)
 	if vals then
 		local r, err = api.set(cgilua.POST.key, vals)
+		if r then
+			err = nil
+		end
 	end
 	if err then
 		put('<br> '..err)
@@ -14,7 +17,7 @@ if cgilua.POST.key then
 		put('<br> DONE')
 	end
 elseif cgilua.QUERY.key then
-	local api = require 'shared.api.configs'
+	local api = require 'shared.api.config'
 	local value, err = api.get(cgilua.QUERY.key)
 	if not value then
 	--	put(err)
