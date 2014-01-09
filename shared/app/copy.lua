891cf7a992f9c5496cb546fc9378a529a5ed7674
cch <srdgame@gmail.com>
2014-01-09 17:58:04 +0800

    do not use the name as copy

--- shared/app/copy.lua
+++
@@ -1,8 +0,0 @@
-
-return function(apps_folder, app, dest)
-	local org_folder = apps_folder..'/'..app.name
-	local new_folder = apps_folder..'/'..dest
-	assert(os.execute('ln -s '..org_folder..' '..new_folder))
-	
-	list.add(dest, app.name, app)
-end
