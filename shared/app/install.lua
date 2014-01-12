0739a5d13df83ca81ec6553ad74aa2a7cd82adc8
cch <srdgame@gmail.com>
2014-01-12 15:39:12 +0800

    fixed the crash on local installation

--- shared/app/install.lua
+++ shared/app/install.lua
@@ -41,7 +41,7 @@ return function(zip_file, apps_folder, dest_name, app)
 	assert(zip_file, 'No application packe file specified')
 	assert(apps_folder, 'No installation folder specified')
 	assert(dest_name, 'No installation name specified')
-	log:debug('APP', "install app", zip_file, apps_folder, dest_name, pp(app))
+	log:debug('APP', "install app", zip_file, apps_folder, dest_name)
 
 	local lock = lfs.lock_dir(apps_folder)
 	if not lock then
