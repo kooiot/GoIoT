3b0a468bb82b6b14b321c3bdad2e62d8342bfaaf
cch <srdgame@gmail.com>
2014-03-05 11:10:12 +0800

    change the name to empft

--- shared/app/empft.lua
+++ shared/app/empft.lua
@@ -1,4 +1,4 @@
-local mpft = {}
+local empft = {}
 
 local function call_handler(name, app, vars)
 	if app.handlers[name] then
@@ -7,20 +7,20 @@ local function call_handler(name, app, vars)
 	return nil, "Not implemented"
 end
 
-mpft['pause'] = function(app, event)
+empft['pause'] = function(app, event)
 	local r, status = call_handler('on_status', app, vars)
 end
 
-mpft['close'] = function(app, event)
+empft['close'] = function(app, event)
 	local r, status = call_handler('on_close', app, vars)
 end
 
-mpft['reload'] = function(app, event)
+empft['reload'] = function(app, event)
 	local r, status = call_handler('on_reload', app, vars)
 end
 
-mpft['info'] = function(app, event)
+empft['info'] = function(app, event)
 	local r, status = app.send_notice()
 end
 
-return mpft
+return empft
