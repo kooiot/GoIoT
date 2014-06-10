92b51610f2df37bdaaa6333a2bafce8dd7a8e227
Dirk Chang <dirk@kooiot.com>
2014-06-10 17:51:56 +0800

    show the error information in web

--- apps/ir/web/controller/index.lua
+++ apps/ir/web/controller/index.lua
@@ -3,15 +3,17 @@ local function doi(req, res, info)
 	local commands = {}
 	local mon = require 'shared.api.mon'
 	local reply, err = mon.query({app.appname})
-	if reply then
+	if reply and reply.status and reply.status[app.appname] then
 		local api = require 'shared.api.app'
-		if reply.status and reply.status[app.appname] then
-			local client = api.new(reply.status[app.appname].port)
-			local reply, err = client:request('list_commands', {})
-			if reply then
-				commands = reply.commands
-			end
+		local client = api.new(reply.status[app.appname].port)
+		local reply, err = client:request('list_commands', {})
+		if reply then
+			commands = reply.commands
+		else
+			info = err
 		end
+	else
+		info = err or 'Status empty'
 	end
 
 	res:ltp('index.html', {lwf=lwf, app=app, commands=commands, info=info})
