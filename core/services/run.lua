5edcc1ca55da173ae000b583f4284c1653cbb0cc
Dirk Chang <dirk@kooiot.com>
2014-06-23 16:28:53 +0800

    show the service name to log

--- core/services/run.lua
+++ core/services/run.lua
@@ -15,20 +15,20 @@ local log = require('shared.log')
 
 local luafile = arg[1] or 'main.lua'
 local srv_name = arg[2] or 'unknown'
-log:info('SRV_RUNNER', 'Start service process from file: '..luafile)
+log:info('SRV_RUNNER', 'Start service['..srv_name..'] process from file: '..luafile)
 
 local f, err =  loadfile(luafile)
 if not f then
-	log:error('SRV_RUNNER', 'Failed to compile service[', pwd, ']:', err)
+	log:error('SRV_RUNNER', 'Failed to compile service[', srv_name, ']:', err)
 else
 	local r, err = pcall(f)
 
 	if not r then
-		log:error('SRV_RUNNER', 'Services exit with err[', pwd, ']:', err)
+		log:error('SRV_RUNNER', 'Services exit with err[', srv_name, ']:', err)
 		local api = require 'shared.api.services'
 		api.result(srv_name, false, err)
 	else
-		log:info('SRV_RUNNER', 'Services exited normally[', pwd, ']')
+		log:info('SRV_RUNNER', 'Services exited normally[', srv_name, ']')
 		local api = require 'shared.api.services'
 		api.result(srv_name, true, 'DONE without error')
 	end
