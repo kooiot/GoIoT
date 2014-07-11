16dfbc221ee2d4ea727f2d18e7bcf32b79dbbfad
Dirk Chang <dirk@kooiot.com>
2014-07-11 16:13:26 +0800

    show the system logs

--- web/www/controller/index.lua
+++ web/www/controller/index.lua
@@ -1,23 +1,39 @@
+
+local SYS_SRC = {
+	['APP'] = 1,
+	['MONITOR'] = 1,
+	['STORE'] = 1,
+	['INSTALL.io'] = 1,
+	['INSTALL.io.conf'] = 1,
+	['WEB'] = 1,
+	['RUNNER'] = 1,
+}
 local function log_to_event(logs)
 	
 	local events = {}
-	for k, v in pairs(logs) do
-		local ev = {}
-		if v.level == 'warn' then
-			ev.label = {icon='green warning'}
-		elseif v.level == 'error' then
-			ev.label = {icon='red attention'}
+	local sys_events = {}
+	for _, v in ipairs(logs) do
+			local ev = {}
+			if v.level == 'warn' then
+				ev.label = {icon='green warning'}
+			elseif v.level == 'error' then
+				ev.label = {icon='red attention'}
+			else
+				ev.label = {icon='teal info'}
+			end
+			ev.date = os.date('%c', v.timestamp / 1000)
+		if not SYS_SRC[v.src] then
+			ev.extra = {
+				text = v.content
+			}
+			ev.summary = '<a href="/apps/'..v.src..'">'..v.src..'</a>'
+			table.insert(events, ev)
 		else
-			ev.label = {icon='teal info'}
+			ev.summary = v.src..' <p>'..v.content
+			table.insert(sys_events, ev)
 		end
-		ev.date = os.date('%c', v.timestamp / 1000)
-		ev.summary = '<a href="/apps/'..v.src..'">'..v.src..'</a>'
-		ev.extra = {
-			text = v.content
-		}
-		table.insert(events, 1, ev)
 	end
-	return events
+	return events, sys_events
 end
 
 local function query_log(limit)
@@ -31,7 +47,7 @@ local function query_log(limit)
 		local flogs = {}
 		for k, v in pairs(logs) do
 			local v = cjson.decode(v)
-			if v and (v.level == 'warn' or v.level == 'error') then
+			if v and (v.level == 'warn' or v.level == 'error' or v.level == 'info') then
 				flogs[#flogs + 1] = v
 			end
 			if limit and #flogs > limit then
@@ -83,10 +99,11 @@ return {
 				}
 			end
 		end
-		local logs, err = query_log(10)
+		local logs, err = query_log(30)
 		local events = {}
+		local sys_events = {}
 		if logs then 
-			events = log_to_event(logs)
+			events, sys_events = log_to_event(logs)
 		else
 			events = {
 				{
@@ -99,6 +116,6 @@ return {
 			}
 		end
 		local settings, err = get_system_settings()
-		res:ltp('index.html', {app=app, lwf=lwf, apps=apps, events=events, settings=settings, err=err})
+		res:ltp('index.html', {app=app, lwf=lwf, apps=apps, events=events, sevents=sys_events, settings=settings, err=err})
 	end
 }
