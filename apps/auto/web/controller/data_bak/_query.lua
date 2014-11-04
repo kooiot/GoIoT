907d0a7b2dda1b3608ed0cb938a2a526b1d89c86
daiokok <jack.dai@symid.com>
2014-11-04 16:37:08 +0800

    smart home

---
+++ apps/auto/web/controller/data_bak/_query.lua
@@ -0,0 +1,38 @@
+return {
+	get = function(req, res)
+		res.headers['Content-Type'] = 'application/json; charset=utf8'
+		local shared = app.model:get('shared')
+
+		local tags = {}
+
+		local api = require('shared.api.iobus.client')
+		local client = api.new('web')
+		local nss, err = client:enum('.+')
+		if nss then
+			for ns, list in pairs(nss) do
+				local tree, err = client:tree(ns)
+				if tree and tree.devices then
+					for _, dev in pairs(tree.devices) do
+						for _, input in pairs(dev.inputs) do
+							local vars, err = client:read(input.path)
+							--[[
+							if vars then
+								for k, v in pairs(vars) do print(k, v) end
+							else
+								print(vars, err)
+							end
+							]]--
+							vars = vars or {timestamp=0}
+							local timestamp = vars.timestamp and  (vars.timestamp / 1000) or nil
+							tags[#tags+1] = {name=vars.path, desc=input.path, value=vars.value or '', timestamp=os.date('%c', timestamp)}
+						end
+					end
+				end
+			end
+		end
+
+		local j = {tags=tags}
+		local cjson = require 'cjson.safe'
+		res:write(cjson.encode(j))
+	end
+}
--- apps/auto/web/controller/index.lua
+++ apps/auto/web/controller/index.lua
@@ -2,129 +2,128 @@ local cjson = require 'cjson.safe'
 platform = require "shared.platform"
 path_plat = platform.path.apps
 
-local function doi(req, res)
+return {
+	get = function(req, res)
+	
 	local path_plat = path_plat .. "/" .. app.appname .. "/config/" .. app.appname .. "/"
+
 	local excute = "mkdir " .. path_plat .. " -p"
 	if not os.execute(excute) then
 		res:write(app.appname)
 		return
 	end
-	local filename = path_plat .. app.appname .. "_config.json"
-	local file, err = io.open(filename, "a+")
-
-	if file then
-		local config = file:read("*a")
-		if config == "" then
-			devices = {}
-			t = {}
-			table.insert(devices,t)
-			config = cjson.encode(devices)
-			file:write(config)
-		end
-		file:close()
-		
-		
-	local api = require 'shared.api.iobus.client'
-	local client = api.new('web')
-	local cjson = require 'cjson.safe'
-
-	local commands = {}
-	local inputs = {}
-
-	local nss, err = client:enum('.+')
-
-	if nss then
-		for ns, devs in pairs(nss) do
-			local tree, err = client:tree(ns)
-
-			if tree then
-				for k, dev in pairs(tree.devices) do
-					for k, v in pairs(dev.commands) do
-						commands[#commands + 1] = {name=v.name, desc=v.desc, path=v.path}
-					end
-					for k, v in pairs(dev.inputs) do
-						inputs[#inputs + 1] = {name = v.name, desc = v.desc, path=v.path}
-					end
-				end
-			end
-		end	
-	else
-		info = err
-	end
-
-	local rules = nil
-	local appapi = require 'shared.api.app'
-	local port, err = appapi.find_app_port(app.appname)
-	if not port then
-		res:write(err or 'Error when trying to find application mgr port')
-	else
-		local client = appapi.new(port)
-		local r, err = client:request('get_rule', {})
-		if r then
-			rules = r.rules
-		end
-	end
 	
-	local conf={}
-	local config_path = require 'shared.api.config'
-	local conf, err = config_path.get(app.appname..'.read') or
-	[[conf.path="/nill/nill/nill"]]
-
-	rules = rules or { ['test1/unit.1/inputs/data2'] = {
-[[ return function(path, value, client) local last_value = GET_LAST_VALUE(path, value.value) if last_value == value.value then return end if value.value == 1 then SEND_CMD('gree\/GREE\/commands\/开机', {'GREE\/开机'}) end end ]]}}
-
-	rules = cjson.encode(rules)
---		res:write(config)
-		res:ltp('index.html', {lwf=lwf, app=app, rules=rules, commands=commands, inputs=inputs,  conf=conf, data_data = 1, json_text=config})
-end
-end
-return {
-	get = 	doi,
-	post = function(req, res)
-
-		req:read_body()
-		local path = path_plat .. "/" .. app.appname .. "/config/" .. app.appname .. "/"
-		local filename = path .. app.appname .. "_config.json"
-		res:write(app.appname)
-		local t = {}
-		t.config={}
-		local ratio = req:get_post_arg("ratio")
-		res:write(ratio)
-		t.config.ratio = cjson.decode(ratio)
-
-		local file, err = io.open(filename, "a+")
-		if not file then
-			res:write(err)
-			return
+	local filename = path_plat .. app.appname .. "_config.json"
+--	res:write(app.appname)
+	
+	local file, err = io.open(filename, "a+")
+	if not file then
+		res:write(err)
+		return
+	end
+	local config = file:read("*a");
+	if config == "" then
+--		res:write("@@@@@@@@@@@@@@@@@@@@")
+		config = '[]'
+	end
+		config = cjson.decode(config)
+	file:close();	
+	len = 0
+	while true do
+		index,value = next(config,index)
+		if index then
+			len = index
+		else
+			break
 		end
---		file:write(ratio)
+	end
+--	res:write(len)
+	local api = require "shared.api.iobus.client"
+	local client = api.new('web')
 
-		local json_text = file:read("*a")
-		local tags = cjson.decode(json_text)
-		local flags = false
-		if tags then	
-			tags = {}
-			for k, v in pairs(tags)	do
-				if v.config.ratio then
-					tags[k] = t
-					flags = true
-			--		res:write ("*********************************")
-					break
-				end
-			end
-			if flags == false then
-				table.insert(tags, t)
+--		res:write(config[2].config.input)
+		local i =0
+		len = tonumber(len)
+	while i < len do
+		if tostring(config[len-i].config.input) ~= "0" then
+			local r, err = client:read(config[len-i].config.input)
+			if r then
+				config[len-i].config.input = r.value
+			else
 			end
 		else
-			table.insert(tags. t)
+			config[len-i].tree.name=""
+			config[len-i].config.input=""
 		end
-	
-		file:close()
-		file = io.open(filename, "w")
-		file:write(cjson.encode(tags))
---]]
-		file:close()
+		i = i+1
+	end
+		
+		config = cjson.encode(config)
+		res:ltp('index.html', {lwf = lwf, app = app,json_text = config})
+	end,
 
+	post = function(req, res)
+
+	local path_plat = path_plat .. "/" .. app.appname .. "/config/" .. app.appname .. "/"
+	local filename = path_plat .. app.appname .. "_config.json"
+--	res:write(app.appname)
+	
+	local file, err = io.open(filename, "a+")
+	if not file then
+		res:write(err)
+		return
+	end
+	local config = file:read("*a");
+	config = cjson.decode(config)
+		
+	file:close();	
+	len = 0
+	while true do
+		index,value = next(config,index)
+		if index then
+			len = index
+		else
+			break
+		end
+	end
+--	res:write(len)
+
+	local api = require "shared.api.iobus.client"
+	local client = api.new('web')
+
+--		res:write(config[2].config.input)
+		local i =0
+		len = tonumber(len)
+	while i < len do
+		if tostring(config[len-i].config.input) ~= "0" then
+			local r, err = client:read(config[len-i].config.input)
+			if r then
+				config[len-i].config.input = r.value
+			else
+			end
+		else
+	--		config[len-i].config.input = "x"
+			config[len-i].tree.name=""
+			config[len-i].config.input=""
+		end
+		i = i+1
+	end
+		
+		config = cjson.encode(config)
+		res:write(config)
 	end
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
