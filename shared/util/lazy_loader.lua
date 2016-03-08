537e7b24f1bd89c989410a153ea50186eb52655a
Dirk Chang <dirk@kooiot.com>
2016-03-08 13:46:37 +0800

    create call function for loader

--- shared/util/lazy_loader.lua
+++ shared/util/lazy_loader.lua
@@ -1,61 +1,55 @@
 local lfs = require 'lfs'
 
+local function _enum_dir (path, cb)
+	for file in lfs.dir(path) do
+		if file ~= "." and file ~= ".." then
+			local f = path..'/'..file
+			local attr = lfs.attributes (f)
+			assert (type(attr) == "table")
+			if attr.mode == "directory" then
+				_enum_dir (f, function(sub)
+					cb(file..'/'..sub)
+				end)
+			else
+				cb(file)
+			end
+		end
+	end
+end
+
+local class = {}
+local function _add_package(self, name, pkname)
+	assert(name)
+	assert(type(pkname) == 'string')
+	self._packages[name] = function() return require(pkname) end
+end
+
+function class:load(base_folder, base_pk)
+	assert(base_folder and base_pk)
+	_enum_dir(base_folder, function(file, ...)
+		local p = file:match('(.+)%.lua$') or file:match('(.+)%.luac$')
+		if p then
+			local pk = p:gsub('/', '.')
+			_add_package(self, pk, base_pk..'.'..pk)
+		end
+	end)
+end
+function class:package(name, package)
+	_add_package(self, name, package or name)
+end
+
+local _load = function(self, name)
+	if not self._packages[name] then
+		return assert(nil, 'package '..name..' does not exits this loader')
+	end
+	self._loaded[name] = self._packages[name]()
+	return self._loaded[name]
+end
+
+local _loader_call = function(self, pkname)
+	return self._loaded[pkname] or _load(self, pkname)
+end
+
 return function()
-	local _packages = {}
-
-	local function _enum_dir (path, cb)
-		for file in lfs.dir(path) do
-			if file ~= "." and file ~= ".." then
-				local f = path..'/'..file
-				local attr = lfs.attributes (f)
-				assert (type(attr) == "table")
-				if attr.mode == "directory" then
-					--[[
-					_enum_dir (f, function(sub)
-						cb(file..'/'..sub)
-					end)
-					]]--
-				else
-					cb(file)
-				end
-			end
-		end
-	end
-
-	local _loaded = {}
-	local function _add_package(name, pkname)
-		assert(not name:match('%.'))
-		assert(type(pkname) == 'string')
-		_packages[name] = function() return require(pkname) end
-	end
-
-	_loaded.load = function(base_folder, base_pk)
-		assert(base_folder and base_pk)
-		_enum_dir(base_folder, function(file)
-			local p = file:match('(.+)%.lua$') or file:match('(.+)%.luac$')
-			if p then
-				local pk = p:gsub('/', '.')
-				_add_package(pk, base_pk..'.'..pk)
-			end
-		end)
-	end
-	_loaded.package = function(name, package)
-		if package and type(package) == 'table' then
-			_loaded[name] = package
-		else
-			_add_package(name, package or name)
-		end
-	end
-
-	local _load = function(name)
-		_loaded[name] = _packages[name]()
-		return _loaded[name]
-	end
-
-	local _loader = function(self, pkname)
-		print(self, pkname)
-		return _loaded[pkname] or _load(pkname)
-	end
-
-	return setmetatable({}, {__index=_loader})
+	return setmetatable({_loaded = {}, _packages={}}, {__index=class, __call=_loader_call})
 end
